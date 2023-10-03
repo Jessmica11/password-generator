4b1bba7a294b86fe6c1a09c33e4024881fb39ac6
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-10-03 11:35:49 -0400

    added comments to breakdown requirements of project

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate window pop-up prompts for different password requirements
+// Requirement 1: At least 8-12 characters, no more than 128
+
+
+// Requirement 2: special characters: numbers
+
+
+// Requirement 3: special characters: uppercase letters 
+
+
+// Requirement 4: special characters: lowercase letters
+
+
+// Requirement 5: special characters: symbols/special characters
+
+
+// If all above "true", then generate output
+
+
+// Randomize output of each option into one long 12-char string
+
+
+// Console.log output of generated password
