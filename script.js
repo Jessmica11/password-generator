0f1e26a8ddb50e368526069232aca116efd52c71
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-10-05 18:38:28 -0400

    collaborated with instructor to simplify my prior code and apply different ideas

--- script.js
+++ script.js
@@ -14,36 +14,88 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //TODO: MY CODE BELOW//
-// Define password requirements as variables
+
+// Used later based on each confirm for each requirement
+var yesUppercase;
+var yesLowercase;
+var yesSymbols;
+var yesNumbers;
+
+// parameters for each password requirement
 var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
 var lowercaseABC ="abcdefghijklmnopqrstuvwxyz";
-var specialSymbols ="!@#$%^&*()?.<\>|=+:;,[-_]";
+var symbols ="!@#$%^&*()?.<\>|=+:;,[-_]";
 var numbers ="0123456789";
-var multiSelect =[uppercaseABC + lowercaseABC + numbers + specialSymbols];
 
-// Confirm prompts for the user to answer aka user choices for password
-var howLong = prompt("How long would you like your password to be? Please choose between 8-128 characters");
-// In case user chooses number outside of scope provided
-if (howLong < 8 || howLong > 128) {
-  alert("Your password does not meet the criteria");
-  var howLong = prompt("Password must be between 8 and 128 characters in length.");
+// defined via confirms by user
+var confirm;
+var userChoices;
+
+function randomIndex (limit) {
+        return Math.floor(Math.random() * limit);
 }
-var uppercase = confirm("Click OK if you would like numbers in your password");
-var lowercase = confirm("Click OK if you would like lowercase letters in your password");
-var specialSymbols = confirm("Click OK if you would like special symbols in your password");
-var numbers = confirm("Click OK if you would like numbers in your password");
+var betweenZeroAndFive = randomIndex(5);
 
-//In case user doesn't pick any option we need an error alert
-if (uppercase === false && lowercase === false && specialSymbols === false && numbers === false) {
-  alert("You must choose at least 1 requirement to generate a password. Please refresh to start over");
-}
-
-// Need to define the generatePassword() function for each requirement's options
+// actual function that will run through all the options
 function generatePassword() {
-    password = "";
-    password += uppercase[Math.floor(Math.random() * uppercase.length)];
-    password += lowercase[Math.floor(Math.random() * lowercase.length)];
-    password += numbers[Math.floor(Math.random() * numbers.length)];
-    password += specialSymbols[Math.floor(Math.random() * specialSymbols.length)];
-    generatePassword.length = howLong;
+        var possibleCharacters = "";
+       // asks for user input
+    var howLong =parseInt(window.prompt("How long would you like your password to be? Please choose between 8-128 characters"));
+    // first if statement for user validation 
+    if (!howLong) {
+        alert("Please submit your desired password length, between 8-128 characters");
+        howLong = null;
+    } else if (howLong < 8 || howLong > 128) {
+        // validates user input
+        // start user input prompts
+        alert("Please choose between 8-128");
+        howLong = null;
+      //  generatePassword();
+        return;
+
+    } else {
+        // ask user yes or no for next set of password requirements
+        yesNumbers = confirm("Click OK if you would like your password to contain numbers");
+        yesUppercase = confirm("Click OK if you would like your password to contain Uppercase letters");
+        yesLowercase = confirm("Click OK if you would like your password to contain Lowercase letters");
+        yesSymbols = confirm("Click OK if you would like your password to contain special characters");
+    };
+
+    // if the user doesn't choose any option, 
+    if (!yesNumbers && !yesUppercase && !yesLowercase && !yesSymbols) {
+        userChoices = alert("You must choose a criteria!");
+    // restart the function
+    generatePassword()
+    }
+    // first if statement that uses user input prompts to determine choices
+  
+    if(yesUppercase) {
+        possibleCharacters = possibleCharacters.concat(uppercaseABC);
+    }
+    if(yesLowercase) {
+        possibleCharacters = possibleCharacters.concat(lowercaseABC);
+    }
+    if(yesNumbers){
+        possibleCharacters = possibleCharacters.concat(numbers);
+    }
+
+    if(yesSymbols){
+        possibleCharacters = possibleCharacters.concat(symbols);
+    }
+
+    console.log(possibleCharacters);
+    console.log(typeof possibleCharacters);
+    possibleCharacters = possibleCharacters.split("");
+
+   // this is an empty array that will be created by the user's choices for the different password requirements
+    var password = "";
+    console.log(howLong);
+    // random selection for all variables: 
+    for (var i = 0; i < howLong; i++) {
+        var pickChoices = possibleCharacters[randomIndex(possibleCharacters.length)];
+        password = password.concat(pickChoices);
+    }
+    console.log(password);
+    return password;
+
 }
